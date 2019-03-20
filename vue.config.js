db58dbd042a48bd398ef8af24aa2e5b534a87595
Richard Lindner <rlindner81@gmail.com>
2019-03-20 07:32:05 +0100

    Version updates

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? process.env.BASE_URL
-    : '/',
+    ? process.env.BASE_URL : '/',
   css: {
     loaderOptions: {
       sass: {
