6898bc4bd30279c5a51881afb451797e52236785
Richard Lindner <rlindner81@gmail.com>
2019-03-25 21:56:13 +0100

    Fix cache busting

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? process.env.BASE_URL : '/',
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true
+    }
+  },
   css: {
     loaderOptions: {
       sass: {
