dad251d10a7145d7a1d2c422631ad6ee65a72b02
Richard Lindner <rlindner81@gmail.com>
2019-03-26 21:21:48 +0100

    Fix PWA home screen version

--- src/router.js
+++ src/router.js
@@ -8,6 +8,10 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    { 
+      path: '/index.html', 
+      redirect: '/' 
+    },
     {
       path: '/',
       name: 'sudoku',
