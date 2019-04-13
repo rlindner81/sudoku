d27ed0790c5907d8c482abe7dac430619a4ffb0f
Richard Lindner <rlindner81@gmail.com>
2019-04-13 13:11:26 +0200

    Naming

--- solver/src/helper.mjs
+++ solver/src/helper.mjs
@@ -118,7 +118,7 @@ export class Counter {
 
   log() {
     let now = new Date();
-    let log = `${this.name} | total ${now - this.startTime}ms | delta ${now - this.lastCallTime}ms | calls ${this.calls++}`;
+    let log = `${this.name} | total ${now - this.startTime}ms | delta ${now - this.lastCallTime}ms | call ${this.calls++}`;
     if (arguments.length > 0) {
       log = Array.prototype.reduce.call(arguments, (result, argument) => {
         return `${result} | ${argument}`;
