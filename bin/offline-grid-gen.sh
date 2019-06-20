ebe3136209db865af2231cb9d8b818d9d94cc214
Richard Lindner <rlindner81@gmail.com>
2019-06-20 20:02:47 +0200

    run em all

--- bin/offline-grid-gen.sh
+++ bin/offline-grid-gen.sh
@@ -5,14 +5,15 @@ script_name=`basename "$script" .sh`
 script_pid="$script_dir/$script_name.pid"
 script_txt="$PWD/$script_name.txt"
 
-stop_script() {
-  if [ -f "$script_pid" ]; then kill `cat "$script_pid"` >/dev/null 2>&1; fi
+stop_start_script() {
+  if [ -f "$script_pid" ]; then kill `cat "$script_dir/$1-$script_name.pid"` >/dev/null 2>&1; fi
+  nohup node --experimental-modules "$script_dir"/../src/offline/generate.mjs --size $1 >"$script_txt" 2>&1 &
+  echo "$!" >"$script_dir/$1-$script_name.pid"
 }
 
-start_script() {
-  nohup node --experimental-modules "$script_dir"/../src/offline/generate.mjs "$@" >"$script_txt" 2>&1 &
-  echo "$!" >"$script_pid"
-}
-
-stop_script
-start_script "$@"
+stop_start_script 9
+stop_start_script 10
+stop_start_script 12
+stop_start_script 14
+stop_start_script 15
+stop_start_script 16
