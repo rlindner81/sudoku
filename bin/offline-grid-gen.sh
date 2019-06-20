1ef5fed69fc8e7508ebb7f40b34eb683a10672c9
Richard Lindner <rlindner81@gmail.com>
2019-06-20 20:04:34 +0200

    Forgot one

--- bin/offline-grid-gen.sh
+++ bin/offline-grid-gen.sh
@@ -6,8 +6,8 @@ script_pid="$script_dir/$script_name.pid"
 script_txt="$PWD/$script_name.txt"
 
 stop_start_script() {
-  if [ -f "$script_pid" ]; then kill `cat "$script_dir/$1-$script_name.pid"` >/dev/null 2>&1; fi
-  nohup node --experimental-modules "$script_dir"/../src/offline/generate.mjs --size $1 >"$script_txt" 2>&1 &
+  if [ -f "$script_dir/$1-$script_name.pid" ]; then kill `cat "$script_dir/$1-$script_name.pid"` >/dev/null 2>&1; fi
+  nohup node --experimental-modules "$script_dir"/../src/offline/generate.mjs --size $1 >"$PWD/$1-$script_name.txt" 2>&1 &
   echo "$!" >"$script_dir/$1-$script_name.pid"
 }
 
