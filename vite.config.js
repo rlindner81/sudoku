4dc1813b10f31099a32bfe3b08aa6c0f2002ad37
Richard Lindner <rlindner81@gmail.com>
2024-10-09 10:35:29 +0200

    Update vite.config.js
    hosted top-level under subdomain

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue2 from "@vitejs/plugin-vue2";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue2()],
-  base: process.env.NODE_ENV === "production" ? "/sudoku/" : "/",
+  // base: process.env.NODE_ENV === "production" ? "/sudoku/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
